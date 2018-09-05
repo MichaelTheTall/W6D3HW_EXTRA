296a4e71518c4720dc29d7159bd183d9d5a6efe9
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-05 17:16:03 +0100

    Ext finished

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,19 @@
 const AnagramFinder = function (word) {
-
+  this.word = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  let checkword = this.word.toLowerCase().split('').sort().join('').trim();
+  let result = [];
 
+for (var i = 0; i < otherWords.length; i++) {
+  let tempword = otherWords[i].toLowerCase().split('').sort().join('').trim();
+  if (otherWords[i] === this.word) {
+
+  } else if (tempword === checkword)
+  result.push(otherWords[i]);
+}
+return result;
 }
 
 module.exports = AnagramFinder;
