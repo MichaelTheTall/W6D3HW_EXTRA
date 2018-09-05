b08e8b721328247462e2d82b1fe30ec6b58a8041
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-05 15:38:08 +0100

    MVP 1

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,10 @@
 const UpperCaser = function (words) {
+  this.words = words;
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+  let result = this.words.map(w=>w.toUpperCase());
+  return result;
 }
 
 module.exports = UpperCaser;
