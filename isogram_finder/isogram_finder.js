dcca3627f97b7cc588cf31f6c876ed491fde6125
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-05 16:59:51 +0100

    MVP3

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,10 +1,28 @@
 const IsogramFinder = function (word) {
-this.word = word;
+  this.word = word.split('');
 }
 
 
 IsogramFinder.prototype.isIsogram = function () {
+  const object = {};
+  const repeats = [];
 
+  this.word.forEach(item => {
+    if(!object[item])
+    object[item] = 0;
+    object[item] += 1;
+  })
+
+  for (const prop in object) {
+    if(object[prop] >= 2) {
+      repeats.push(prop);
+    }
+  }
+  if (repeats.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = IsogramFinder;
